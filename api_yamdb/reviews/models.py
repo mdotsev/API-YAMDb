cdad607b1b0c79ecb9fdbd3d27b5f7f5b25b90f7
Давид Амиров <david.talish@yandex.ru>
2022-07-15 19:51:05 +0300

    Добавление модели Жанра, Категории, Загаловка

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,8 @@
+import datetime
+
 from django.contrib.auth import get_user_model
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 
 SCORES = (
     (1, "One"),
@@ -18,9 +20,6 @@ SCORES = (
 
 User = get_user_model()
 
-
-class Title(models.Model):
-    pass
 
 
 class Review(models.Model):
@@ -56,3 +55,50 @@ class Comment(models.Model):
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
+
+
+class Category(models.Model):
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=256)
+    year = models.PositiveIntegerField(
+        verbose_name='Дата выхода',
+        validators=[MaxValueValidator(datetime.date.today().year)]
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True
+    )
+    rating = models.IntegerField(
+        verbose_name='Рейтинг',
+        null=True,
+        default=None
+    )
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True)
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}, жанр - {self.genre}'
