7527eec8484ab17292d5d8a8c44c2ad0891d72f5
Max <m@dotsev.ru>
2022-07-16 13:31:40 +0300

    successful-start

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,3 +7,12 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
         )
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_authenticated
+                    and (request.user.is_admin or request.user.is_superuser)
+                    )
+                )
