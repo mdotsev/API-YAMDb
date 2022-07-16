6f48d1eb8950ba36c18567b4454b78f1d5f08b3c
Max <m@dotsev.ru>
2022-07-16 09:13:49 +0300

    Models, Serializers, Views, Endpoints created. API and Titles apps registered, settings created

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,4 +1,21 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from .views import CommentViewSet, ReviewViewSet
+
 app_name = 'api'
+
+router_v1 = routers.DefaultRouter()
+
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/',
+    ReviewViewSet, basename='reviews'
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet, basename='comments'
+)
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+]
